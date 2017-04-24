981027097d403520ff9fe35fdacad20f7af74cc8
revolutions-jls <joesgot2bcool@gmail.com>
2017-04-24 14:25:58 -0700

    added main code
    basic code for this project.

---
+++ main-flow.py
@@ -0,0 +1,202 @@
+import time
+y = "y"
+n = "n"
+g = "g"
+r = "r"
+# sean is garbage at coding
+# eat it sean
+gbfwycfd = "Go back from whence you came, foul Demon!"
+safety = "You are doing okay. No revolution here."
+print ("the")
+time.sleep(1.5)
+print ("revolutionary")
+time.sleep(1.5)
+print ("revolution")
+time.sleep(1.5)
+print ("flowchart")
+time.sleep(1.5)
+print ("made by Joe Salmon, Sean Hong, and Luke Dunn")
+time.sleep(1.5)
+print ("enjoy")
+time.sleep(1.5)
+print ("-- -- -- --")
+time.sleep(1.5)
+religionfree = raw_input("Are your people free to follow their ideals? [y/n] : ")
+if religionfree == y:
+        govcontrol = raw_input("Does the government have totalitarian-esque control over the people? [y/n] : ")
+        if govcontrol == y:
+            needsrespond = raw_input("Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
+            if needsrespond == y:
+                print ("There are a few anti-totalitarianists about. You may need more propaganda.")
+            elif needsrespond == n:
+                econstatus = raw_input(
+                    "Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
+                if econstatus == y:
+                    outsidesupp = raw_input(
+                        "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva de la Revolucion!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y:
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+                    if outsidesupp == n:
+                        print ("There is some civil unrest.")
+                if econstatus == n:
+                    outsidesupp = raw_input(
+                        "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva de la Revolucion!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y:
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+                    if outsidesupp == n:
+                        print ("There is some civil unrest.")
+        elif govcontrol == n:
+            needsrespond = raw_input(
+                "Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
+            if needsrespond == y:
+                print ("There are a few anti-totalitarianists about. You may need more propaganda.")
+            elif needsrespond == n:
+                econstatus = raw_input(
+                    "Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
+                if econstatus == y:
+                    outsidesupp = raw_input(
+                        "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva de la Revolucion!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y:
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+                    if outsidesupp == n:
+                        print ("There is some civil unrest.")
+                if econstatus == n:
+                    print gbfwycfd
+elif religionfree == n:
+        govcontrol = raw_input("Does the government have totalitarian-esque control over the people? [y/n] : ")
+        if govcontrol == y:
+            needsrespond = raw_input("Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
+            if needsrespond == y:
+                print ("There is some civil unrest.")
+            elif needsrespond == n:
+                econstatus = raw_input("Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
+                if econstatus == y:
+                    outsidesupp = raw_input("Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva la Revolution!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y :
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+                    if outsidesupp == n:
+                        print ("There is some civil unrest.")
+                if econstatus == n:
+                    outsidesupp = raw_input(
+                        "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva la Revolution!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y:
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+        elif govcontrol == n:
+            needsrespond = raw_input("Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
+            if needsrespond == y:
+                if govcontrol == y:
+                    if needsrespond == y:
+                        print ("A lack of general freedom will probably cause some civil unrest.")
+                    elif needsrespond == n:
+                        econstatus = raw_input(
+                            "Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
+                        if econstatus == y:
+                            outsidesupp = raw_input(
+                                "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                            if outsidesupp == y:
+                                suppreceive = raw_input(
+                                    "Are the rebels or the government receiving the support? [r/g] : ")
+                                if suppreceive == r:
+                                    print ("Viva de la Revolucion!")
+                                if suppreceive == g:
+                                    oppose = raw_input(
+                                        "Are there people opposing the government / current power? [y/n] : ")
+                                    if oppose == y:
+                                        print ("A possible civil war. Stockpile.")
+                                    if oppose == n:
+                                        print ("There is some civil unrest.")
+                elif govcontrol == n:
+                    print ("A lack of idealistic freedom may cause civil unrest.")
+            elif needsrespond == n:
+                econstatus = raw_input("Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
+                if econstatus == y:
+                    outsidesupp = raw_input("Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva de la Revolucion!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y:
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+                    if outsidesupp == n:
+                        outsidesupp = raw_input(
+                            "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                        if outsidesupp == y:
+                            suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                            if suppreceive == r:
+                                print ("Viva de la Revolucion!")
+                            if suppreceive == g:
+                                oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                                if oppose == y:
+                                    print ("A large amount of the people are unhappy, presumably with the lack of ability to make idealistic choices and the government's lack of response. With a little more motivation, revolution or even civil war could occur.")
+                                if oppose == n:
+                                    print ("An amount of the people are unhappy, and this could lead to other things given the right push.")
+                if econstatus == n:
+                    outsidesupp = raw_input(
+                        "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                    if outsidesupp == y:
+                        suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
+                        if suppreceive == r:
+                            print ("Viva de la Revolucion!")
+                        if suppreceive == g:
+                            oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
+                            if oppose == y:
+                                print ("A possible civil war. Stockpile.")
+                            if oppose == n:
+                                print ("There is some civil unrest.")
+                    if outsidesupp == n:
+                        outsidesupp = raw_input(
+                            "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+                        if outsidesupp == y:
+                            suppreceive = raw_input("Are the rebels or the government receiving the support? [r/g] : ")
+                            if suppreceive == r:
+                                print ("Viva de la Revolucion!")
+                            if suppreceive == g:
+                                oppose = raw_input("Are there people actively opposing the government / current power? [y/n] : ")
+                                if oppose == y:
+                                    print ("A large amount of the people are unhappy, presumably with the lack of ability to make idealistic choices and the government's lack of response. With a little more motivation, revolution or even civil war could occur.")
+                                if oppose == n:
+                                    print ("An amount of the people are unhappy, and this could lead to other things given the right push.")
