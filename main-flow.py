bf2b95449971bf014cb4ddb2a4e7b89089b297f1
revolutions-jls <joesgot2bcool@gmail.com>
2017-04-24 21:38:48 -0700

    fixed an issue with accidentally having a question repeat itself + other fun things
    i swear python is nice and all but UGH

--- main-flow.py
+++ main-flow.py
@@ -14,57 +14,47 @@ suppreceive = 7
 gbfwycfd = "Go back from whence you came, foul Demon!"
 safety = "You are doing okay. No revolution here."
 print ("the")
-time.sleep(1.5)
+time.sleep(1)
 print ("revolutionary")
-time.sleep(1.5)
+time.sleep(1)
 print ("revolution")
-time.sleep(1.5)
+time.sleep(1)
 print ("flowchart")
-time.sleep(1.5)
+time.sleep(1)
 print ("made by Joe Salmon, Sean Hong, and Luke Dunn")
-time.sleep(1.5)
+time.sleep(1)
 print ("enjoy")
-time.sleep(1.5)
+time.sleep(1)
 print ("-- -- -- --")
-time.sleep(1.5) #git test
-religionfree = raw_input("Are your people free to follow their ideals? [y/n] : ")
+time.sleep(1) #git test
 while True:
+    religionfree = raw_input("Are your people free to follow their ideals? [y/n] : ")
     if religionfree == y:
-        break
         while True:
             govcontrol = raw_input("Does the government have totalitarian-esque control over the people? [y/n] : ")
             if govcontrol == y:
-                break
                 while True:
                     needsrespond = raw_input("Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
                     if needsrespond == y:
-                        break
                         print ("There are a few anti-totalitarianists about. You may need more propaganda.")
                     elif needsrespond == n:
-                        break
                         while True:
                             econstatus = raw_input("Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
                             if econstatus == y:
-                                break
                                 while True:
                                     outsidesupp = raw_input("Can either party [either the rebels or the government] procure outside support? [y/n] : ")
                                     if outsidesupp == y:
-                                        break
                                         while True:
                                             suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
                                             if suppreceive == r:
                                                 print ("Viva de la Revolucion!")
-                                                break
                                             elif suppreceive == g:
-                                                break
                                                 while True:
                                                     oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
                                                     if oppose == y:
                                                         print ("A possible civil war. Stockpile.")
-                                                        break
                                                     elif oppose == n:
                                                         print "[that's not what I asked you]"
-                                                        break
                                                     else:
                                                         print "[that's not what I asked you]"
                                                         continue
@@ -85,33 +75,25 @@ while True:
                     needsrespond = raw_input("Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
                     if needsrespond == y:
                         print ("There are a few anti-totalitarianists about. You may need more propaganda.")
-                        break
                     elif needsrespond == n:
-                        break
                         while True:
                             econstatus = raw_input(
                                 "Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
                             if econstatus == y:
-                                break
                                 while True:
                                     outsidesupp = raw_input(
                                         "Can either party [either the rebels or the government] procure outside support? [y/n] : ")
                                     if outsidesupp == y:
-                                        break
                                         while True:
                                             suppreceive = raw_input("Are the rebels or the government recieving the support? [r/g] : ")
                                             if suppreceive == r:
-                                                break
                                                 print ("Viva de la Revolucion!")
                                             elif suppreceive == g:
-                                                break
                                                 while True:
                                                     oppose = raw_input("Are there people opposing the government / current power? [y/n] : ")
                                                     if oppose == y:
-                                                        break
                                                         print ("A possible civil war. Stockpile.")
                                                     if oppose == n:
-                                                        break
                                                         print ("There is some civil unrest.")
                                                     else:
                                                         print "[that's not what I asked you]"
@@ -248,7 +230,11 @@ while True:
                                     if oppose == y:
                                         print ("A large amount of the people are unhappy, presumably with the lack of ability to make idealistic choices and the government's lack of response. With a little more motivation, revolution or even civil war could occur.")
                                     if oppose == n:
-                                        print ("An amount of the people are unhappy, and this could lead to other things given the right push.")
+                                        print (
+                                        "An amount of the people are unhappy, and this could lead to other things given the right push.")
+            else:
+                print "[that's not what I asked you]"
+                continue
     else:
         print "[that's not what I asked you]"
         continue
