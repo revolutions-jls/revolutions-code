4e87ff57ce3025d8d7449f22f3be7709bfee4bb9
walmart-givenchii <joesgot2bcool@gmail.com>
2017-05-15 20:28:30 -0700

    dded option to skip intro. possibly pointless

--- revflow.py
+++ revflow.py
@@ -4,21 +4,23 @@ import time
 #cause you can't really test a rocket before firing
 #so i think this works
 #but i dont want to fire it for fear it doesn't
-print ("the")
-time.sleep(1.5)
-print ("revolutionary")
-time.sleep(1.5)
-print ("revolution")
-time.sleep(1.5)
-print ("flowchart")
-time.sleep(1.5)
-print ("(now with functions)")
-time.sleep(1.5)
-print ("made by Joe Salmon, Sean Hong, and Luke Dunn")
-time.sleep(1.5)
-print ("enjoy")
-time.sleep(1.5)
-print ("-- -- -- --")
+intro = raw_input("intro? [y/n]")
+if intro == "y":
+    print ("the")
+    time.sleep(1.5)
+    print ("revolutionary")
+    time.sleep(1.5)
+    print ("revolution")
+    time.sleep(1.5)
+    print ("flowchart")
+    time.sleep(1.5)
+    print ("(now with functions)")
+    time.sleep(1.5)
+    print ("made by Joe Salmon, Sean Hong, and Luke Dunn")
+    time.sleep(1.5)
+    print ("enjoy")
+    time.sleep(1.5)
+    print ("-- -- -- --")
 time.sleep(1.5)
 if r.religionfree():
     if r.govcontrol():
