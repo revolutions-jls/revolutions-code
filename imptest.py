3e6a259705b617a69126323ec17633771b13a494
walmart-givenchii <joesgot2bcool@gmail.com>
2017-05-22 11:30:04 -0700

    Revert "cleaned up branch and its a little better"
    This reverts commit 54ee5a26c4255e0984fe8abdba6bff4e83261671.

---
+++ imptest.py
@@ -0,0 +1,27 @@
+import rev as r
+
+if r.religionfree():
+    if r.govcontrol():
+        if r.needsrespond():
+            if r.econstatus():
+                if r.outsidesupp():
+                    print "cool"
+                else:
+                    p
+            else:
+                if r.outsidesupp():
+                else:
+        else:
+    else:
+else:
+    if r.govcontrol():
+        if r.needsrespond():
+            if r.econstatus():
+                if r.outsidesupp():
+                else:
+            else:
+        else:
+    else:
+
+
+else:
