8241d331d8db20ffb19b04ebeda05620858fe587
walmart-givenchii <joesgot2bcool@gmail.com>
2017-05-22 11:06:18 -0700

    so i cleaned up
    this place is a mess

---
+++ academics/case_studies.md
@@ -0,0 +1,6 @@
+#Our Case Studies
+#the proof is in ~the pudding~ academics\case_studies.md
+
+To actually prove that this is correct, we realized we had to do several, small case studies. These are nothing exceptional, and merely serve as a collection of facts that we've manipulated to make people think this actually works. Our subjects are
+
+ Out of our group of three people, we've each taken two case studies, with the exception of Sean Hong, who's just doing the French Revolution, because it's quite a complex one. Joe Salmon has taken the fall of the
---
+++ dependencies/rev.py
@@ -0,0 +1,83 @@
+def religionfree():
+    while True:
+        answer = raw_input("Are your people free to follow their ideals? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+
+def govcontrol():
+    while True:
+        answer = raw_input("Does the government have totalitarian-esque control over the people? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+#tacobucks
+def needsrespond():
+    while True:
+        answer = raw_input("Is the government responding to the needs of a sufficient portion of the people? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+
+def econstatus():
+    while True:
+        answer = raw_input("Is the economy failing or on a very apparent downturn? Are there high taxes or unfair labor laws? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+
+def outsidesupp():
+    while True:
+        answer = raw_input("Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+
+def supprecieve():
+    while True:
+        answer = raw_input("Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+
+def oppose():
+    while True:
+        answer = raw_input("Can either party [either the rebels or the government] procure outside support? [y/n] : ")
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print "[i'm not sure what you meant by that]"
+            continue
+
+
+
+
+
+
+
